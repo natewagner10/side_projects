dd419d3e36edf195de40b803f1834de5a8652fc5
natewag10 <natewag1234@gmail.com>
2019-12-04 17:02:27 -0500

    wrote program to find what sets you can build given a set or sets

--- program1.py
+++ program1.py
@@ -16,11 +16,6 @@ cur = conn.cursor()
 setnums = input("Please enter the set_num associated with the set you have, seperated by a comma: ")
 setnums = "0013-1, 0013-10, 0055-1, 00222-1, 0009995-1"
 
-setnums = str(setnums)
-if len(setnums) > 1:
-    setnums = setnums.split(",")
-    setnums = [x.replace(' ', '') for x in setnums]
-
 
 # clean the user input
 setnums = setnums.split(",")
@@ -105,6 +100,5 @@ for i in range(1, len(ALLPartsList)):
 cur.execute("select set_num, set_name, sum/num_parts::float*100 as percentParts from (select a.set_num, a.sum, b.set_name, b.num_parts from (select a.set_num, b.sum from inventories a, (select inventory_id, sum(quantity) from (" + GetParts + ") hold group by hold.inventory_id order by sum desc) b where a.inventory_id = b.inventory_id) a, sets b where a.set_num = b.set_num) a order by percentParts desc limit 10;")
 InventoryID_SumParts = cur.fetchall()
 print(InventoryID_SumParts)
-
 
 
